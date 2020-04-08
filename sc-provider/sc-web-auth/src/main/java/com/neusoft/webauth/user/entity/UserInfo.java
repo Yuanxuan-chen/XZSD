cf1e779df6717b3589631aee728e1cbf5d1a9825
Yuanxuan <1045010589@qq.com>
2020-04-08 17:40:02 +0800

    适配user的createUser的接口

--- sc-provider/sc-web-auth/src/main/java/com/neusoft/webauth/user/entity/UserInfo.java
+++ sc-provider/sc-web-auth/src/main/java/com/neusoft/webauth/user/entity/UserInfo.java
@@ -45,7 +45,7 @@ public class UserInfo {
     /**
      * 电子邮件
      */
-    private String email;
+    private String mail;
     /**
      * IP地址
      */
@@ -177,11 +177,11 @@ public class UserInfo {
     }
 
     public String getEmail() {
-        return email;
+        return mail;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.mail = email;
     }
 
     public String getIp() {
